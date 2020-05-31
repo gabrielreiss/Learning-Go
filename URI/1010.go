41a5bcbe8a7daf32eab1a8957e452518f9bb732b
gabrielreiss <gabrielreissdecastro@gmail.com>
2020-05-31 00:48:52 -0300

    1010

--- URI/1010.go
+++ URI/1010.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 )
 
+type cad struct {
+	cod int
+	num int
+	vl  float64
+}
+
+func entrada() cad {
+	var cod, num int
+	var vl float64
+	fmt.Scanf("%d %d %f\n", &cod, &num, &vl)
+	return cad{cod, num, vl}
+}
+
+func Total(cad1 cad, cad2 cad) float64 {
+	total := float64(cad1.num)*cad1.vl + float64(cad2.num)*cad2.vl
+	return total
+}
+
 func main() {
-	var cod1, cod2, num1, num2 int
-	var vl1, vl2 float64
 
-	fmt.Scanf("%d %d %f\n", &cod1, &num1, &vl1)
-	fmt.Scanf("%d %d %f", &cod2, &num2, &vl2)
+	cad1 := entrada()
+	cad2 := entrada()
 
-	total := float64(num1)*vl1 + float64(num2)*vl2
+	total := Total(cad1, cad2)
 
 	fmt.Printf("VALOR A PAGAR: R$ %.2f\n", total)
 }
